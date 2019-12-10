7cc3c6c2e32526b56675d09e7b9f588155bbf4b4
Luis Angel Rodriguez Reiners <luis.rodriguezreiners@gmail.com>
2019-12-10 16:30:11 +0100

    first commit

---
+++ OpenGL/src/IndexBuffer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class IndexBuffer
+{
+public:
+	IndexBuffer(const unsigned int* data, unsigned int count);
+	~IndexBuffer();
+
+	void Bind() const;
+	void Unbind() const;
+
+private:
+	unsigned int m_RenderedID;
+	unsigned int m_Count;
+
+	inline unsigned int GetCount() const { return m_Count; }
+};
